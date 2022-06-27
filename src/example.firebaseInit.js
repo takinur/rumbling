6f0517a19b274d5925dca317c804990ad7f7ae9c
TAKINUR <takinurm@gmail.com>
2022-06-27 12:54:08 +0600

    Added Firebase

---
+++ src/example.firebaseInit.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: '',
+  authDomain: '',
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
