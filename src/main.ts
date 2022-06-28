d756cd6d05b0baeec390cd0d575ea82eaef9af14
TAKINUR <takinurm@gmail.com>
2022-06-29 02:59:22 +0600

    Animated Randomize

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,4 @@ import './style.css'
 
 const app = createApp(App)
 
-app.use(router)
-
-app.mount('#app')
+app.use(router).mount('#app')
--- src/views/Candidates.vue
+++ src/views/Candidates.vue
@@ -1,37 +1,102 @@
 <template>
-  <div class="h-screen text-center items-center justify-center">
-    <h1 class="text-green-500">{{ msg }}</h1>
-    <h2 class="h-16 text-green-500">Is tailwind enabled?</h2>
-  </div>
-  <div ref="dropdown" class="dropdown">
-    <strong class="dropdown-label" @click="show = !show"
-      >Click me to open!</strong
-    >
-    <p class="dropdown-content" v-if="show">Lorum ipsum...</p>
+  <div class="example list-example">
+    <ul ref="list">
+      <li v-for="item in items" :key="item">
+        <span>{{ item }}</span>
+        <button @click.prevent="remove(item)" aria-label="Remove Fruit">
+          <IconRemove />
+        </button>
+      </li>
+    </ul>
+
+    <button class="button button--add button--alt" @click="add">
+      + Add Fruit
+    </button>
+    <button class="button button--random button--alt" @click="randomize">
+      Randomize
+    </button>
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from "vue";
+<script setup lang="ts">
 import { ref, onMounted } from "vue";
 import autoAnimate from "@formkit/auto-animate";
-
-const dropdown = ref(); // we need a DOM node
-const show = ref(false);
-
-export default defineComponent({
-  name: "Candidates",
-  data() {
-    return {
-      msg: "Hello Vue.js!",
-    };
-  },
-  setup() {
-    onMounted(() => {
-      autoAnimate(dropdown.value); // thats it!
-    });
-  },
-});
+const items = ref(["🍎 Apple", "🍌 Banana", "🍒 Cherry"]);
+const list = ref<HTMLElement>();
+const fruitBasket = [
+  "🍓 Strawberry",
+  "🥥 Coconut",
+  "🥝 Kiwi",
+  "🍇 Grape",
+  "🍉 Watermelon",
+  "🍍 Pineapple",
+  "🍐 Pear",
+  "🍑 Peach",
+  "🫐 Blueberry",
+  "🍊 Orange",
+  "🥭 Mango",
+];
+const remove = (item) => {
+  items.value = items.value.filter((fruit) => {
+    if (fruit === item) {
+      fruitBasket.push(fruit);
+      return false;
+    }
+    return true;
+  });
+};
+const add = () => {
+  if (fruitBasket.length) {
+    items.value.splice(
+      Math.round(Math.random() * items.value.length - 1),
+      0,
+      fruitBasket.shift()
+    );
+  } else {
+    alert("Out of fruit!");
+  }
+};
+const randomize = () => items.value.sort(() => (Math.random() > 0.5 ? 1 : -1));
+onMounted(() => autoAnimate(list.value));
 </script>
 
-<style></style>
+<style scoped>
+ul {
+  list-style-type: none;
+  padding: 0;
+  max-width: 300px;
+}
+li {
+  display: flex;
+  justify-content: space-between;
+  padding: 0.75em;
+  background-color: white;
+  margin-bottom: 0.5em;
+  border-radius: 0.5em;
+  box-shadow: 0 0 0.5em rgba(0, 0, 0, 0.1);
+  font-size: 0.875em;
+}
+[data-dark-mode="true"] li {
+  background-color: var(--purple-md);
+}
+li::before {
+  display: none;
+}
+li button {
+  appearance: none;
+  border: none;
+  padding: none;
+  margin: none;
+  background-color: transparent;
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+}
+li button svg {
+  width: 1.2em;
+  fill: red;
+}
+[data-dark-mode="true"] li button svg {
+  fill: rgb(244, 67, 67);
+}
+</style>
