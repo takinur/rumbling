728f1b8d262f8270fcd95d9258b74b84721a4916
TAKINUR <takinurm@gmail.com>
2022-07-23 02:24:48 +0600

    Added Error Handleing for Suspense

--- src/views/Candidates.vue
+++ src/views/Candidates.vue
@@ -1,15 +1,15 @@
 <script setup lang="ts">
-import { ref, defineAsyncComponent } from "vue";
+import { ref, defineAsyncComponent, onErrorCaptured } from "vue";
 import AccrodionVue from "../components/Accrodion.vue";
-// import MyAsyncComponenetVue from "../components/MyAsyncComponent.vue";
 // import { ContentLoader } from "vue-content-loader";
 import LoadingVue from "../components/Loading.vue";
+import MyAsyncComponentVue from "../components/MyAsyncComponent.vue";
 
-const MyAsyncComponenetVue = defineAsyncComponent({
-  loader: () => import("../components/MyAsyncComponent.vue"),
-  // loadingComponent: LoadingVue,
-  suspensible: true
-});
+// const MyAsyncComponenetVue = defineAsyncComponent({
+//   loader: () => import("../components/MyAsyncComponent.vue"),
+//   // loadingComponent: LoadingVue,
+//   suspensible: true
+// });
 
 const items = [
   {
@@ -28,6 +28,11 @@ const items = [
     content: "eh this is a test",
   },
 ];
+const error : any = ref(null);
+onErrorCaptured((e) => {
+  error.value = e;
+  return true;
+});
 </script>
 
 <template>
@@ -36,12 +41,15 @@ const items = [
       <h2 class="text-xl font-semibold text-vnet-blue mb-2 text-center">
         Accrodion --VUE Tailwind
       </h2>
-      <accrodion-Vue :items="items" />
+      <AccrodionVue :items="items" />
     </main>
-    <!--Async-->
+    <!--Async Suspense with Err handling-->
+    <div v-if="error" class="text-red-400">
+      Uh oh ... {{  error }}
+    </div>
     <Suspense>
       <template #default>
-        <my-async-componenet-Vue />
+        <MyAsyncComponentVue />
       </template>
       <template #fallback>
          <loading-vue />
