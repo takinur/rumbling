dbce3fffa092738db046c576dc768a007886a4ee
TAKINUR <takinurm@gmail.com>
2022-07-23 11:46:18 +0600

    Added Select Component for Form

---
+++ src/components/BaseSelect.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+const props = defineProps({
+  label: {
+    type: String,
+    default: "",
+  },
+  modelValue: {
+    type: [String, Number],
+    default: "",
+  },
+  options: {
+    type: Array,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <label v-if="label">{{ label }}</label>
+  <select
+    class="field"
+    :value="modelValue"
+    v-bind="{
+      ...$attrs,
+      onChange: ($event) => {
+        $emit('update:modelValue', $event.target.value);
+      },
+    }"
+  >
+    <option
+      v-for="option in options"
+      :value="option"
+      :key="option"
+      :selected="option === modelValue"
+    >
+      {{ option }}
+    </option>
+  </select>
+</template>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,11 +1,37 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import BaseSelectVue from "@/components/BaseSelect.vue";
+
+const categories = ref([
+  "Fruits",
+  "Vegetables",
+  "Dairy",
+  "Grains",
+  "Meats",
+  "Drinks",
+  "Sweets",
+  "Others",
+]);
+
+const form = ref({
+  category: "",
+  name: "",
+  description: "",
+});
+
+
+
+</script>
+
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
-
-    <h2 class="h-16 text-green-500">Is tailwind enabled?</h2>
+    <h1 text-green-500>This is an about page</h1>
+    <BaseSelectVue
+      :options="categories"
+      v-model="form.category"
+      label="Select a category"
+    />
   </div>
 </template>
 
-<style>
-
-</style>
+<style></style>
